498eddee659ba4d57fc705ddd2fcd233b4527479
DESKTOP-MI4T68R\msi <srunkerry@gmail.com>
2025-08-10 11:23:59 +0700

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import CheckOut from "./pages/CheckOut";
 import { Route, Routes } from "react-router-dom";
 function App() {
